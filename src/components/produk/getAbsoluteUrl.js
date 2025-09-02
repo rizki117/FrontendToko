aad40f427928018fc616505281b79bb26209d8b5
rizki117 <rizkisetian7@gmail.com>
2025-09-02 23:25:15 +0700

    update produk tambah

--- src/components/produk/getAbsoluteUrl.js
+++ src/components/produk/getAbsoluteUrl.js
@@ -1,7 +1,14 @@
 export const getAbsoluteUrl = (path) => {
   if (!path) return '';
   if (path.startsWith('http://') || path.startsWith('https://')) return path;
-  // hapus semua / di depan dulu, lalu tambahkan satu /
+
   const cleanedPath = path.replace(/^\/+/, '');
+  
+  // kalau di localhost → pakai window.location.origin
+  if (window.location.hostname === "localhost") {
+    return `${window.location.origin}/${cleanedPath}`;
+  }
+
+  // kalau di deploy (Vercel)
   return `https://frontend-toko-ten.vercel.app/${cleanedPath}`;
 };
--- src/components/produk/produk.js
+++ src/components/produk/produk.js
@@ -28,6 +28,120 @@ sellerPhone: "085881385710",
     imageUrl: "/images/henset.jpeg",
     sellerPhone: "085881385710", 
   },
+  
+  //di ambil dari folder kaos
+{
+    title: "Kaos Btc",
+    description: "Kaos distro minimalis ini cocok untuk kamu yang suka tampilan simple tapi keren. Potongan modern dan bahan katun lembut memberikan kenyamanan maksimal.",
+    price: 245000,
+    imageUrl: "/kaos/btc.jpeg",
+    sellerPhone: "085881385710", 
+  },  
+  
+  
+{
+    title: "Jaket FITE",
+    description: "Kaos distro minimalis ini cocok untuk kamu yang suka tampilan simple tapi keren. Potongan modern dan bahan katun lembut memberikan kenyamanan maksimal.",
+    price: 365000,
+    imageUrl: "/kaos/jaket1.jpeg",
+    sellerPhone: "085881385710", 
+  },    
+  
+  
+{
+    title: "Jaket Vgt",
+    description: "Kaos distro minimalis ini cocok untuk kamu yang suka tampilan simple tapi keren. Potongan modern dan bahan katun lembut memberikan kenyamanan maksimal.",
+    price: 385000,
+    imageUrl: "/kaos/jaket2.jpeg",
+    sellerPhone: "085881385710", 
+  },    
+  
+  
+{
+    title: "Kaos Gtk",
+    description: "Kaos distro minimalis ini cocok untuk kamu yang suka tampilan simple tapi keren. Potongan modern dan bahan katun lembut memberikan kenyamanan maksimal.",
+    price: 255000,
+    imageUrl: "/kaos/kaos1.jpeg",
+    sellerPhone: "085881385710", 
+  },       
+  
+  
+{
+    title: "Kaos Ckh",
+    description: "Kaos distro minimalis ini cocok untuk kamu yang suka tampilan simple tapi keren. Potongan modern dan bahan katun lembut memberikan kenyamanan maksimal.",
+    price: 265000,
+    imageUrl: "/kaos/kaos2.jpeg",
+    sellerPhone: "085881385710", 
+  },      
+  
+  {
+    title: "Kaos Blk",
+    description: "Kaos distro minimalis ini cocok untuk kamu yang suka tampilan simple tapi keren. Potongan modern dan bahan katun lembut memberikan kenyamanan maksimal.",
+    price: 245000,
+    imageUrl: "/kaos/kaos3.jpeg",
+    sellerPhone: "085881385710", 
+  },   
+  
+  
+{
+    title: "Kaos Brd",
+    description: "Kaos distro minimalis ini cocok untuk kamu yang suka tampilan simple tapi keren. Potongan modern dan bahan katun lembut memberikan kenyamanan maksimal.",
+    price: 275000,
+    imageUrl: "/kaos/kaos4.jpeg",
+    sellerPhone: "085881385710", 
+  },     
+  
+  
+  {
+    title: "Kaos Cho",
+    description: "Kaos distro minimalis ini cocok untuk kamu yang suka tampilan simple tapi keren. Potongan modern dan bahan katun lembut memberikan kenyamanan maksimal.",
+    price: 255000,
+    imageUrl: "/kaos/kaos5.jpeg",
+    sellerPhone: "085881385710", 
+  },   
+   
+   
+ {
+    title: "Kaos Bnb",
+    description: "Kaos distro minimalis ini cocok untuk kamu yang suka tampilan simple tapi keren. Potongan modern dan bahan katun lembut memberikan kenyamanan maksimal.",
+    price: 265000,
+    imageUrl: "/kaos/kaos6.jpeg",
+    sellerPhone: "085881385710", 
+  },   
+     
+  
+  
+ {
+    title: "Sandal Slop",
+    description: "Sandal ini dirancang dengan material berkualitas yang ringan dan tahan lama, memberikan kenyamanan maksimal saat digunakan.",
+    price: 80000,
+    imageUrl: "/sandal/sandal1.jpeg",
+    sellerPhone: "085881385710", 
+  },   
+  
+  
+  {
+    title: "Sandal Slop",
+    description: "Sandal ini dirancang dengan material berkualitas yang ringan dan tahan lama, memberikan kenyamanan maksimal saat digunakan.",
+    price: 85000,
+    imageUrl: "/sandal/sandal2.jpeg",
+    sellerPhone: "085881385710", 
+  },   
+  
+  
+  {
+    title: "Sandal Slop",
+    description: "Sandal ini dirancang dengan material berkualitas yang ringan dan tahan lama, memberikan kenyamanan maksimal saat digunakan.",
+    price: 75000,
+    imageUrl: "/sandal/sandal3.jpeg",
+    sellerPhone: "085881385710", 
+  },   
+  
+     
+    
+   
+  
+  
 ];
 
 export default produk;  
